016f8cef836d7f89b7fbd2e3048c3ac45c44640e
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-05-06 11:26:54 +0530

    saving refresh tokens

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,12 +16,20 @@ module.exports = {
             return res.status(403).json({ msg: "Forbidden" });
           }
 
-          OAuth2Client.setCredentials({ access_token: decoded.access_token });
+          OAuth2Client.setCredentials({
+            access_token: decoded.access_token,
+            refresh_token: decoded.refresh_token,
+          });
 
           const {
             data: { id },
           } = await profile.userinfo.get();
 
+          /**
+           * check in database by the google id
+           * if present, set it in req.user
+           * if not create a user and then set it to req.user
+           */
           req.userId = id;
           next();
         }
